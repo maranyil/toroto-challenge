5b29653ee7beaff857b346f6d37210684a040677
Mara Mulato <maramulato@MacBook-Mara.local>
2021-02-23 04:32:50 -0600

    market place area done

--- toroto/src/pages/Compra.js
+++ toroto/src/pages/Compra.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import CreditCardInput from 'react-credit-card-input';
+import hojita from '../assets/img/hojita.jpg';
 
 const Compra = () => {
-      constructor(props) {
-        super(props);
-        this.state = {
-          username: '',
-          age: null,
-        };
-      }
-      myChangeHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        this.setState({[nam]: val});
-      }
-        return (
-          <form>
-          <h1>Hello {this.state.username} {this.state.age}</h1>
-          <p>Enter your name:</p>
-          <input
-            type='text'
-            name='username'
-            onChange={this.myChangeHandler}
-          />
-          <p>Enter your age:</p>
-          <input
-            type='text'
-            name='age'
-            onChange={this.myChangeHandler}
-          />
-          </form>
-        );
-    };
+
+
+    const [datos, setDatos] = useState({
+        nombre: '',
+        apellido: '',
+        cantidad: '',
+        date: new Date(),
+        país: ''
+    })
+
+    const handleInputChange = (event) => {
+        setDatos({
+            ...datos,
+            [event.target.name] : event.target.value
+        })
+    }
     
+
+    const enviarDatos = (event) => {
+        event.preventDefault()
+    }
+
+    return (
+        <Fragment>
+            
+            <form className="formC" onSubmit={enviarDatos}>
+            <h1>Compra bonos de carbono con Toroto</h1>
+                <div className="datosCompra">
+                    <input type="text" placeholder="Nombre" className="form-control" onChange={handleInputChange} name="nombre"></input>
+                    <input type="text" placeholder="Apellido" className="form-control" onChange={handleInputChange} name="apellido"></input>
+                    <input type="number" placeholder="Cantidad de bonos" className="form-control" onChange={handleInputChange} name="cantidad"></input>
+                    <p>*Como parte de la prevención ante el doble gasto, te pedimos incluír tu Estado y País.<br/>
+                    Esto impide que otras personas se lleven el crédito por tus bonos de carbono.</p>
+                    <input type="text" placeholder="Estado, País" className="form-control" onChange={handleInputChange} name="pais"></input><br/>
+                    <CreditCardInput/><br/>
+                    <button type="submit" className="btn btn-primary">Enviar</button>
+                </div>      
+            </form>
+ 
+            <div className="verificado">
+                <h2>Bono de Carbono Toroto</h2>
+                <img src={hojita}/>
+                <p>Este certificado confirma que {datos.nombre} {datos.apellido}</p>
+                <p>Realizó la compra de {datos.cantidad} bonos de carbono</p>
+                <p>El día {datos.date.toLocaleDateString()}</p>
+                <h4>Serial: xxxx-xxxx-xxxx</h4>
+
+            </div>
+        </Fragment>
+    );
+}
 
 export default Compra;
